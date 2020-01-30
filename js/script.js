7e0daf2c89261474fb505ef66bebaefa46f2783d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-01-30 00:51:42 -0300

    Typed Js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $("#slides").superslides({
         animation: 'fade',
         play: 5000,
         pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer", "PHP", "Laravel", "Student"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
     });
 });
