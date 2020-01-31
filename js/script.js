c774b16e3b45e46a02763e6292fb0adc3825a7e7
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-01-30 23:14:14 -0300

    Activating the charts on scroll

--- js/script.js
+++ js/script.js
@@ -34,15 +34,25 @@ $(document).ready(function() {
         }
     });
 
-    $('.chart').easyPieChart({
-        easing: 'easeInOut',
-        barColor: '#fff',
-        trackColor: false,
-        scaleColor: false,
-        lineWidth: 4,
-        size: 152,
-        onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    // console.log(skillsTopOffset);
+    $(window).scroll(function() {
+        // console.log(window.pageYOffset);
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+
         }
+
     });
 });
